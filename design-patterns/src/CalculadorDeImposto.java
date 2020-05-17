6d38fde4c352329e8f4515cc4d1a14042a381f09
anaalicecosta <anaalice.cd@gmail.com>
2020-05-17 19:35:36 -0300

    inclusao do projeto design patterns

---
+++ design-patterns/src/CalculadorDeImposto.java
@@ -0,0 +1,8 @@
+
+public class CalculadorDeImposto {
+	public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer) {
+		double imposto = impostoQualquer.calcula(orcamento);
+		System.out.println(imposto);
+
+	}
+}
